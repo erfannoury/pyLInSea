0e43fbf08853045e541fc2d63d00b2e98d151875
erfannoury <erfannoury@users.noreply.github.com>
2014-12-05 22:04:47 +0330

    initial searcher implementation

---
+++ search.py
@@ -0,0 +1,66 @@
+import numpy as np
+import scipy as sc
+import pandas as pd
+import matplotlib.pyplot as plt
+import re, os, sys
+import tarfile
+from datetime import datetime
+
+import lucene
+from java.io import File
+from org.apache.lucene.analysis.standard import StandardAnalyzer
+from org.apache.lucene.index import DirectoryReader
+from org.apache.lucene.queryparser.classic import QueryParser
+from org.apache.lucene.store import SimpleFSDirectory
+from org.apache.lucene.search import IndexSearcher
+from org.apache.lucene.util import Version
+
+document_folder = '/home/erfan/Desktop/mirproject/pylucene/cran/'
+document_name = 'cran.all.1400'
+document_path = os.path.join(document_folder, document_name)
+
+index_folder = '/home/erfan/Desktop/mirproject/pylucene'
+index_name = 'cran.index'
+index_path = os.path.join(index_folder, index_name)
+
+# Options
+TokenizeFields = True
+
+#field names:
+title_field = 'title'
+author_field = 'author'
+content_field = 'content'
+
+
+def search(searcher, analyzer):
+	query = QueryParser(Version.LUCENE_CURRENT, content_field, analyzer).parse('temperature and velocity')
+	scoreDocs = searcher.search(query, 20).scoreDocs
+	
+	print 'total matching docs: ', len(scoreDocs)
+	
+	for scoreDoc in scoreDocs:
+		doc = searcher.doc(scoreDoc.doc)
+		print '%s\n' % doc.get(title_field)
+		print 'by %s\n' % doc.get(author_field)
+		print '%s \n' % doc.get(content_field)
+		
+		print '#######################'
+
+
+def main():
+	
+	lucene.initVM()
+	print 'lucene version ', lucene.VERSION	
+	version = Version.LUCENE_CURRENT
+	directory = SimpleFSDirectory(File(index_path))
+	searcher = IndexSearcher(DirectoryReader.open(directory))
+	analyzer = StandardAnalyzer(version)
+	
+	search(searcher, analyzer)
+	
+	del searcher
+
+
+
+if __name__ == '__main__':
+	main()
