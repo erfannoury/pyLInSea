07d225462c6d0d495c066da5200be7d922b44d98
erfannoury <erfannoury@users.noreply.github.com>
2014-12-07 22:17:00 +0330

    Now using the docID field in the search module

--- search.py
+++ search.py
@@ -4,7 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import re, os, sys
 import tarfile
-from datetime import datetime
+from datetime import datetime as dt
 
 import lucene
 from java.io import File
@@ -15,11 +15,11 @@ from org.apache.lucene.store import SimpleFSDirectory
 from org.apache.lucene.search import IndexSearcher
 from org.apache.lucene.util import Version
 
-document_folder = '/home/erfan/Desktop/mirproject/pylucene/cran/'
+document_folder = 'cran/'
 document_name = 'cran.all.1400'
 document_path = os.path.join(document_folder, document_name)
 
-index_folder = '/home/erfan/Desktop/mirproject/pylucene'
+index_folder = '.'
 index_name = 'cran.index'
 index_path = os.path.join(index_folder, index_name)
 
@@ -30,35 +30,39 @@ TokenizeFields = True
 title_field = 'title'
 author_field = 'author'
 content_field = 'content'
-
+docid_field = 'docid'
 
 def search(searcher, analyzer):
 	query = QueryParser(Version.LUCENE_CURRENT, content_field, analyzer).parse('temperature and velocity')
 	scoreDocs = searcher.search(query, 20).scoreDocs
-	
+
 	print 'total matching docs: ', len(scoreDocs)
-	
+
 	for scoreDoc in scoreDocs:
 		doc = searcher.doc(scoreDoc.doc)
+		print 'docid ', doc.get(docid_field)
 		print '%s\n' % doc.get(title_field)
 		print 'by %s\n' % doc.get(author_field)
 		print '%s \n' % doc.get(content_field)
-		
+
 		print '#######################'
 
 
 def main():
-	
 	lucene.initVM()
-	print 'lucene version ', lucene.VERSION	
+	print 'lucene version ', lucene.VERSION
 	version = Version.LUCENE_CURRENT
 	directory = SimpleFSDirectory(File(index_path))
 	searcher = IndexSearcher(DirectoryReader.open(directory))
 	analyzer = StandardAnalyzer(version)
-	
+
+	start = dt.now()
 	search(searcher, analyzer)
-	
+	end = dt.now()
 	del searcher
+
+	print 'elapsed time searching:'
+	print end - start
 
 
 
